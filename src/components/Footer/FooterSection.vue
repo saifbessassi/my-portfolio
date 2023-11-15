b7e1edf4ef317e467387382bf989b52fe040f70f
Saif BESSASSI <sa.bessassi@gmail.com>
2023-11-15 16:49:58 +0100

    fix footer responiavity

--- src/components/Footer/FooterSection.vue
+++ src/components/Footer/FooterSection.vue
@@ -2,16 +2,15 @@
   <footer class="bg-background">
     <v-container class="pa-0">
       <v-row justify="space-between" align="center" class="pa-15">
-        <v-col cols="4">
+        <v-col sm="6" md="4" cols="12">
           <p class="mb-3">{{ $t('footer.message') }} 😉</p>
           <p class="text-center">
-            <v-btn class="mr-1" icon="mdi-facebook" variant="text" />
-            <v-btn class="mr-1" icon="mdi-linkedin" variant="text" />
-            <v-btn icon="mdi-github" variant="text" />
+            <v-btn href="https://www.linkedin.com/in/saifbessassi" target="_blanc" class="mr-1" icon="mdi-linkedin" variant="text" />
+            <v-btn href="https://github.com/saifbessassi" target="_blanc" icon="mdi-github" variant="text" />
           </p>
         </v-col>
 
-        <v-col cols="4">
+        <v-col sm="6" md="4" cols="12">
           <h3 class="text-uppercase mb-5">saif bessassi</h3>
           <p class="mb-3"><v-icon class="mr-2">mdi-email</v-icon>sa.bessassi@gmail.com</p>
           <p class="mb-3"><v-icon class="mr-2">mdi-phone</v-icon>+33 6 38 01 14 77</p>
